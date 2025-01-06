675e95b8f3854e14db615b705a41252f28940fe9
Maestro <alnikyur@gmail.com>
2025-01-07 00:49:02 +0200

    1

--- .github/workflows/scripts/prebuild.sh
+++ .github/workflows/scripts/prebuild.sh
@@ -10,10 +10,11 @@ sudo apt update \
   && apt install -y libfontconfig unzip \
   && unzip ${GODOT_VERSION}
 
-mv ${GODOT_VERSION} /usr/local/bin/godot && godot --version
+mv ${GODOT_VERSION} /usr/local/bin/godot
+chmod +x /usr/local/bin/godot && godot --version
 
 # Install godot templates
 wget https://github.com/godotengine/godot-builds/releases/download/4.2-stable/Godot_v4.2-stable_export_templates.tpz
 unzip Godot_v4.2-stable_export_templates.tpz
 mkdir -p ~/.local/share/godot/export_templates/4.2.stable/
-cp -r templates/* ~/.local/share/godot/export_templates/4.2.stable/
+cp -rv templates/* ~/.local/share/godot/export_templates/4.2.stable/
